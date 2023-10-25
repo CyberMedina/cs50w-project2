6a30cbb96a2c8f5974d588d36d85f7962ed3b77c
CyberMedina <jhonatanmedina5255@gmail.com>
2023-10-25 00:57:52 -0600

    Agregando mostrar salas y guardando conversaciones a listas (incompleto)

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
-import os
+import os, datetime
 
 from flask import Flask, render_template, redirect, url_for, request, g, session
 from flask_session import Session
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,7 +12,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.jinja_env.globals['g'] = g
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -20,14 +20,17 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
+MAX_MESSAGES = 100
+channels = set()
+connversations = []
+
+
+
 @app.before_request
 def before_request():
     if 'userName' in session:
 
         g.name = session['userName'] 
-
-    
-
 
 
 @app.route("/",methods= ["GET", "POST"])
@@ -40,6 +43,49 @@ def index():
         return redirect(url_for('index'))
     
     return render_template('index.html')
+
+
+@socketio.on('join')
+def on_join(data):
+    room = data['room']
+    channels.add(room)
+    join_room(room)
+    emit('status', {'msg': 'Te has unido a la sala' + room + '!'})
+
+@socketio.on('get_channels')
+def get_channels():
+    print(channels)
+    print("Entró!")
+    emit('channel_list', list(channels))
+
+@socketio.on('leave')
+def on_leave(data):
+    room = data['room']
+    leave_room(room)
+    emit('status', {'msg' : 'Te has salido de la sala: ' + room + '!'})
+
+@socketio.on('message')
+def handle_message(data):
+    room = data['room']
+    message = data['message']
+    datetimeStatus = datetime.datetime.today().strftime("%Y-%m-%d a las %H:%M:")
+
+    # Almacenando el mensaje en la lista de conversaciones
+    connversations.append({
+        'msg': message, 
+                     'user': session['userName'], 
+                     'datetime': datetime.datetime.today().strftime("%Y-%m-%d a las %H:%M:")
+    })
+
+    if len(connversations) > MAX_MESSAGES:
+        connversations.pop(0)
+
+    print(connversations)
+    emit('message', {'msg': message, 
+                     'user': session['userName'], 
+                     'datetime': datetimeStatus }, room=room)
+
+
 
 @app.route("/close_session")
 def close_session():
